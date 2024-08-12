2cab714b09de17e48fdee0cce5bf84f1d47a9cf8
Garima Majhi <garima691wiz@gmail.com>
2024-08-12 22:25:02 +0530

    updated

--- frontend/src/components/MyBookings.jsx
+++ frontend/src/components/MyBookings.jsx
@@ -14,8 +14,9 @@ const MyBookings = () => {
   }, []);
 
   return (
-    <div className="flex items-center justify-center bg-gradient-to-r from-purple-600 to-indigo-500">
-      <div className="bg-white p-4 rounded-lg shadow-lg max-w-sm w-72 h-full">
+    <div className="flex items-center justify-center  bg-gradient-to-r from-purple-600 to-indigo-500">
+  <div className="bg-white p-4 rounded-lg shadow-lg max-w-sm w-full sm:w-72 h-full overflow-hidden">
+  
         <h1 className="text-3xl font-bold text-center text-indigo-700 mb-3 underline">
           My Bookings
         </h1>
